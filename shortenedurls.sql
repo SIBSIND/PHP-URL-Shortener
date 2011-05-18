45f67d9be9c671f741bba1508196ffbf304a4312
Laurie Denness <laurie@denness.net>
2011-05-18 16:27:26 +0000

    As it turns out, 255 characters aren't going to cut it for most long URLs. Time for a text field.

--- shortenedurls.sql
+++ shortenedurls.sql
@@ -4,13 +4,12 @@
 
 CREATE TABLE `shortenedurls` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `long_url` varchar(255) NOT NULL,
-  `short_url` varchar(255) NOT NULL,
+  `long_url` TEXT NOT NULL,
+  `short_url` varchar(10) NOT NULL,
   `created` int(10) unsigned NOT NULL,
   `creator` char(15) NOT NULL,
   `referrals` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
-  UNIQUE KEY `long` (`long_url`),
   UNIQUE KEY `short` (`short_url`),
   KEY `referrals` (`referrals`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
